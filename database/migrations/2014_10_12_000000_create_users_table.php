05e2905c0a5f448de6c6c834bef196c3094d034a
tomato <aul11cat@gmail.com>
2020-07-04 08:05:59 +0000

    add   User Restful Api && Controller   jwt DB dataTable

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('姓名');
+            $table->string('email')->unique()->comment('電子信箱');
+            $table->string('password');
+            $table->integer('status')->comment('使用者狀態');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
